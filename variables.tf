ae271021d8f9e42d0dc2f8380d33e19fb30165c1
krithikasharma-OT <sevda.sharma@opstree.com>
2025-03-26 19:14:22 +0530

    tf code

---
+++ variables.tf
@@ -0,0 +1,35 @@
+variable "aws_region" {
+  default = "ap-south-1"
+}
+
+variable "jenkins_ami_id" {
+  default = "ami-0e35ddab05955cf57"
+}
+
+variable "jenkins_instance_type" {
+  default = "t2.medium"
+}
+
+variable "jenkins_disk_space" {
+  default = "20"
+}
+
+variable "jenkins_key_name" {
+  default = "Opstree"
+}
+
+variable "SonarNexusServer_instance_ami_id" {
+  default = "ami-0e35ddab05955cf57"
+}
+
+variable "SonarNexusServer_instance_type" {
+  default = "t2.large"
+}
+
+variable "SonarNexusServer_disk_space" {
+  default = "20"
+}
+
+variable "SonarNexusServer_instance_key_name" {
+  default = "Opstree"
+}
