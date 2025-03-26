32a9bdfe498561c863eccef80602191c2836e734
krithikasharma-OT <sevda.sharma@opstree.com>
2025-03-26 19:13:46 +0530

    Create sonarNexusSetup.sh

---
+++ sonarNexusSetup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install -y ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+
+sudo docker --version
+sudo systemctl status docker
+
+sudo docker run -d --name sonarscanner -p 9000:9000  sonarqube:lts-community
+
+sudo docker run -d --name Nexus -p  8081:8081  dockette/nexus
