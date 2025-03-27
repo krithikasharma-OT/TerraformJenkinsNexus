bb9004505075eafebfeef60937e0b870bdfefa3c
krithikasharma-OT <sevda.sharma@opstree.com>
2025-03-27 16:10:03 +0530

    Update jenkinsTrivySetup.sh

--- jenkinsTrivySetup.sh
+++ jenkinsTrivySetup.sh
@@ -37,6 +37,23 @@ sudo apt install -y unzip
 unzip awscliv2.zip
 sudo ./aws/install
 
+echo "Install Docker..."
+sudo apt-get update
+sudo apt-get install -y ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+
 echo "Installing Kubectl..."
 curl -o kubectl https://amazon-eks.s3.us-west-2.amazonaws.com/1.19.6/2021-01-05/bin/linux/amd64/kubectl
 chmod +x ./kubectl
