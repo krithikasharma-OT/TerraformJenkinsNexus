38d7f67ac825e5df39df68aa18c8212a7da4ff64
krithikasharma-OT <sevda.sharma@opstree.com>
2025-03-27 16:45:35 +0530

    Update jenkinsTrivySetup.sh

--- jenkinsTrivySetup.sh
+++ jenkinsTrivySetup.sh
@@ -52,7 +52,7 @@ echo \
 sudo apt-get update
 
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-sudo usermod -aG docker $USER
+sudo usermod -aG docker jenkins
 sudo systemctl restart docker
 
 echo "Installing Kubectl..."
