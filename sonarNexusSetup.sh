2368f81f70f086b1652850d65b930d46f478f30f
krithikasharma-OT <sevda.sharma@opstree.com>
2025-03-28 12:00:34 +0530

    Update sonarNexusSetup.sh

--- sonarNexusSetup.sh
+++ sonarNexusSetup.sh
@@ -22,3 +22,20 @@ sudo systemctl status docker
 sudo docker run -d --name sonarscanner -p 9000:9000  sonarqube:lts-community
 
 sudo docker run -d --name Nexus -p  8081:8081  dockette/nexus
+
+
+echo "Installing Mongodb..."
+sudo apt-get install -y gnupg curl
+curl -fsSL https://www.mongodb.org/static/pgp/server-8.0.asc | \
+   sudo gpg -o /usr/share/keyrings/mongodb-server-8.0.gpg \
+   --dearmor
+
+echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-8.0.gpg ] https://repo.mongodb.org/apt/ubuntu noble/mongodb-org/8.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-8.0.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
+
+sudo systemctl start mongod
+sudo systemctl enable mongod
+
+sudo sed -i 's/\(bindIp:\) 127.0.0.1/\1 0.0.0.0/' /etc/mongod.conf
+sudo systemctl restart mongod
