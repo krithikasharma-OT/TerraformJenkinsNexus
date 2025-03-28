4f36febf58411263814c71210cb6119ab7fc423b
krithikasharma-OT <sevda.sharma@opstree.com>
2025-03-28 10:31:02 +0530

    Update jenkinsTrivySetup.sh

--- jenkinsTrivySetup.sh
+++ jenkinsTrivySetup.sh
@@ -25,18 +25,6 @@ echo deb https://aquasecurity.github.io/trivy-repo/deb $(lsb_release -sc) main |
 sudo apt-get update
 sudo apt-get install -y trivy
 
-echo "Installing Terrafrom..."
-wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
-sudo apt update && sudo apt install -y terraform
-terraform --version
-
-echo "Installing AWS CLI..."
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-sudo apt install -y unzip
-unzip awscliv2.zip
-sudo ./aws/install
-
 echo "Installing Docker..."
 sudo apt-get update
 sudo apt-get install -y ca-certificates curl
