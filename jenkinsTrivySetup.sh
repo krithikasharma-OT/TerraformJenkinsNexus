b162d9a4966a4795a4fe7eb7d642085dc1d2bd0b
krithikasharma-OT <sevda.sharma@opstree.com>
2025-04-01 13:06:56 +0530

    Update jenkinsTrivySetup.sh

--- jenkinsTrivySetup.sh
+++ jenkinsTrivySetup.sh
@@ -43,6 +43,14 @@ sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plug
 sudo usermod -aG docker jenkins
 sudo systemctl restart docker
 
+echo "Installing aws cli..."
+sudo usermod -aG sudo jenkins 
+sudo apt install -y unzip
+sudo apt update && sudo apt install -y python3 python3-pip
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+
 echo "Installing Kubectl..."
 curl -o kubectl https://amazon-eks.s3.us-west-2.amazonaws.com/1.19.6/2021-01-05/bin/linux/amd64/kubectl
 chmod +x ./kubectl
