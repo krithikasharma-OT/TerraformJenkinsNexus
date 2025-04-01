6a7c23bd620fe2e42c5393b79b6ea0db9b620f98
krithikasharma-OT <sevda.sharma@opstree.com>
2025-04-01 17:33:25 +0530

    Update MongoDbSetup.sh

--- MongoDbSetup.sh
+++ MongoDbSetup.sh
@@ -27,8 +27,9 @@ use $MONGO_ADMIN_DB;
 db.createUser({
     user: "$MONGO_USERNAME",
     pwd: "$MONGO_PASSWORD",
-    roles: [{ role: "dbAdmin", db: "$MONGO_DB_NAME" }]
+    roles: [{ role: "readWrite", db: "$MONGO_DB_NAME" }, { role: "dbAdmin", db: "$MONGO_DB_NAME" }]
 });
+
 EOF
 
 
